3aa17ab1c2cf036d18ff9ad224e75abd7b000304
cppmonger <keskinemirhan@gmail.com>
2023-12-06 19:38:18 +0300

    Fix - bind exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { MainExceptionFilter } from "./controllers/exception/exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new MainExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle(process.env["APP_NAME"] || "my-application")
@@ -18,7 +21,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(
     Number(process.env["APP_PORT"]),
-    process.env["APP_HOSTNAME"],
+    process.env["APP_HOSTNAME"]
   );
 }
 bootstrap();
